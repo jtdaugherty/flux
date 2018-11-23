7c180c494cf990dbf9ad0b97c4010b3ddaa2febf
Jonathan Daugherty <cygnus@foobox.com>
2018-11-23 13:14:54 -0800

    More sketching

--- flux-core/src/engine.rs
+++ flux-core/src/engine.rs
@@ -2,8 +2,12 @@
 use rand::Rng;
 
 use scene::Scene;
+use color::Color;
 
-struct JobID(usize, usize);
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Eq)]
+pub struct JobID(usize, usize);
 
 pub struct JobIDAllocator {
     allocator_id: usize,
@@ -27,6 +31,7 @@ impl JobIDAllocator {
     }
 }
 
+#[derive(Debug)]
 pub struct WorkUnit {
     row_start: usize,
     row_end: usize,
@@ -60,24 +65,62 @@ pub struct RenderManager {
 }
 
 impl RenderManager {
-    pub fn new(workers: Vec<Box<Worker>>) -> RenderManager {
+    pub fn new(_workers: Vec<Box<Worker>>) -> RenderManager {
         RenderManager {
             job_id_allocator: JobIDAllocator::new(),
         }
     }
 
-    pub fn schedule_job(&mut self, j: Job) -> JobID {
+    pub fn schedule_job(&mut self, _j: Job) -> JobID {
         self.job_id_allocator.next()
     }
 
-    pub fn cancel_job(&mut self, id: JobID) {
+    pub fn cancel_job(&mut self, _id: JobID) {
     }
 }
 
 pub trait Worker {
     fn set_job(&mut self, j: Job, id: JobID);
-    fn render(&mut self, u: WorkUnit);
+    fn render(&mut self, u: WorkUnit) -> bool;
+}
+
+pub trait RowHandler {
+    fn row_ready(&mut self, id: JobID, row_index: usize, v: Vec<Color>);
 }
 
 pub struct LocalWorker {
+    current_job: Option<(JobID, Job)>,
+    row_handler: Box<RowHandler>,
+    // Need to store a work queue of work units that gets populated by
+    // render() and gets consumed by worker thread
+}
+
+impl LocalWorker {
+    fn new(handler: Box<RowHandler>) -> LocalWorker {
+        LocalWorker {
+            current_job: None,
+            row_handler: handler,
+        }
+    }
+}
+
+impl Worker for LocalWorker {
+    fn set_job(&mut self, j: Job, id: JobID) {
+        self.current_job = Some((id, j));
+    }
+
+    fn render(&mut self, u: WorkUnit) -> bool {
+        match &self.current_job {
+            Some((j_id, j)) => {
+                if *j_id == u.job_id {
+                    println!("LocalWorker: got work unit {:?} for current job", u);
+                    self.row_handler.row_ready(u.job_id, 0, vec![]);
+                    true
+                } else {
+                    false
+                }
+            },
+            None => false
+        }
+    }
 }
