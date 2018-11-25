984f550fe6ea5a417343276a6499302d60e96a16
Jonathan Daugherty <cygnus@foobox.com>
2018-11-25 10:06:20 -0800

    Add CameraSettings

--- fluxcore/src/trace.rs
+++ fluxcore/src/trace.rs
@@ -86,3 +86,21 @@ impl Intersectable for Sphere {
         }
     }
 }
+
+pub struct CameraSettings {
+    pub eye: Vector3<f64>,
+    pub look_at: Vector3<f64>,
+    pub up: Vector3<f64>,
+    pub u: Vector3<f64>,
+    pub v: Vector3<f64>,
+    pub w: Vector3<f64>,
+}
+
+impl CameraSettings {
+    pub fn new(eye: Vector3<f64>, look_at: Vector3<f64>, up: Vector3<f64>) -> CameraSettings {
+        let w = (eye - look_at).normalize();
+        let u = up.cross(&w).normalize();
+        let v = w.cross(&u);
+        CameraSettings { eye, look_at, up, u, v, w }
+    }
+}
