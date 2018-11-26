fe7810aa8fc4cbb6766eec841483b39fd2bc3a24
Jonathan Daugherty <cygnus@foobox.com>
2018-11-25 16:20:14 -0800

    flux: add live rendering window

--- flux/src/main.rs
+++ flux/src/main.rs
@@ -1,6 +1,14 @@
 
 extern crate fluxcore;
 extern crate nalgebra;
+extern crate sdl2;
+
+use std::time::Duration;
+
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::image::{INIT_PNG, INIT_JPG};
 
 use nalgebra::{Point3, Vector3};
 
@@ -12,9 +20,9 @@ use fluxcore::color::Color;
 
 fn main() {
     let c = JobConfiguration {
-        rows_per_work_unit: 100,
+        rows_per_work_unit: 10,
         max_trace_depth: 1,
-        sample_root: 1,
+        sample_root: 10,
     };
     let s = SceneData {
         scene_name: String::from("test_scene"),
@@ -58,6 +66,32 @@ fn main() {
         ],
     };
 
+    // SDL setup /////////////////////////////////////////////////////////////
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(INIT_PNG | INIT_JPG).unwrap();
+
+    let image_width = s.output_settings.image_width;
+    let image_height = s.output_settings.image_height;
+    let window = video_subsystem.window("flux render",
+                                        image_width as u32,
+                                        image_height as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+    let mut texture = texture_creator.create_texture_streaming(
+        PixelFormatEnum::RGB24,
+        image_width as u32,
+        image_height as u32
+        ).unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    //////////////////////////////////////////////////////////////////////////
+
     println!("Starting local worker");
     let worker = LocalWorker::new();
     let image_builder = ImageBuilder::new();
@@ -67,6 +101,65 @@ fn main() {
 
     println!("Sending job to rendering manager");
     let job = manager.schedule_job(s, c);
+
+    let mut copied_rows: Vec<bool> = (0..image_height).map(|_| false).collect();
+    let mut finished = false;
+
+    'running: loop {
+        {
+            if !finished {
+                let img_ref = image_builder.get_image();
+                let mut opt = img_ref.lock().unwrap();
+                match opt.as_mut() {
+                    None => {
+                        println!("No texture yet.");
+                    },
+                    Some(img) => {
+                        println!("Updating texture");
+                        let mut num_skipped_rows = 0;
+                        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                            for y in 0..image_height {
+                                if !copied_rows[y] {
+                                    let ps = &img.pixels[y];
+
+                                    if ps.len() > 0 {
+                                        for (x, pixel) in ps.iter().enumerate() {
+                                            let offset = y*pitch + x*3;
+                                            buffer[offset] = (pixel.r * 255.99) as u8;
+                                            buffer[offset + 1] = (pixel.g * 255.99) as u8;
+                                            buffer[offset + 2] = (pixel.b * 255.99) as u8;
+                                        }
+                                        copied_rows[y] = true;
+                                    }
+                                } else {
+                                    println!("Skipping row {}", y);
+                                    num_skipped_rows += 1;
+                                }
+                            }
+                        }).unwrap();
+                        if num_skipped_rows == image_height {
+                            finished = true;
+                        }
+                    },
+                }
+            }
+        }
+
+        canvas.copy(&texture, None, None).expect("Render failed");
+        canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running
+                    },
+                _ => {}
+            }
+        }
+
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 
     println!("Awaiting job completion");
     job.wait();
