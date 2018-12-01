5b3ce66964f08cd4f61109fc9f8ea20b94d1c38b
Jonathan Daugherty <cygnus@foobox.com>
2018-12-01 11:22:40 -0800

    Remove unused methods for Matte

--- fluxcore/src/materials.rs
+++ fluxcore/src/materials.rs
@@ -15,21 +15,6 @@ pub struct Matte {
     pub diffuse_brdf: Lambertian,
 }
 
-impl Matte {
-    pub fn set_ka(&mut self, k: f64) {
-        self.ambient_brdf.diffuse_coefficient = k;
-    }
-
-    pub fn set_kd(&mut self, k: f64) {
-        self.diffuse_brdf.diffuse_coefficient = k;
-    }
-
-    pub fn set_color(&mut self, c: Color) {
-        self.diffuse_brdf.diffuse_color = c;
-        self.ambient_brdf.diffuse_color = c;
-    }
-}
-
 impl Material for Matte {
     fn path_shade(&self, scene: &Scene, hit: &Hit, samples: &MasterSampleSets,
                   set_index: usize, sample_index: usize) -> Color {
