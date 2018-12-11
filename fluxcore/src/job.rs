e0fca25ef17a4d91f1095ab76a9bac21218c867d
Jonathan Daugherty <cygnus@foobox.com>
2018-12-11 11:42:59 -0800

    Add serde (de)serializers for job types

--- fluxcore/src/job.rs
+++ fluxcore/src/job.rs
@@ -45,6 +45,7 @@ pub struct WorkUnit {
 
 #[derive(Clone)]
 #[derive(Copy)]
+#[derive(Serialize, Deserialize)]
 pub struct JobConfiguration {
     pub sample_root: usize,
     pub max_trace_depth: usize,
@@ -54,6 +55,7 @@ pub struct JobConfiguration {
 // A job provides all the resources and configuration needed to render a
 // scene.
 #[derive(Clone)]
+#[derive(Serialize, Deserialize)]
 pub struct Job {
     pub id: JobID,
     pub scene_data: SceneData,
